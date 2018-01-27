30ab860dfdf0bca01a607910bf31d8540d411224
9utell <9utell@gmail.com>
2018-01-27 08:41:03 -0800

    Adding and configure API project.

---
+++ Dashboard.API/Config/AutofacWebAPI.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Dashboard.DAL.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Dashboard.API.Config
+{
+    public class AutofacWebAPI
+    {
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterService(new ContainerBuilder()));
+        }
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+        private static IContainer RegisterService(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // EF DbContext
+            builder.RegisterType<EntityContext>().As<DbContext>().InstancePerApiRequest();
+            //Repositories
+
+            //Services
+            return builder.Build();
+        }
+    }
+}
---
+++ Dashboard.API/Config/WebAPIConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.Validation;
+using System.Web.Http.Validation.Providers;
+
+namespace Dashboard.API.Config
+{
+    public class WebAPIConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            //Formatters
+            var jqueryFormatter = config.Formatters.FirstOrDefault(_ => _.GetType() == typeof(JQueryMvcFormUrlEncodedFormatter));
+
+            config.Formatters.Remove(config.Formatters.FormUrlEncodedFormatter);
+            config.Formatters.Remove(jqueryFormatter);
+
+            foreach(var formatter in config.Formatters)
+            {
+                formatter.RequiredMemberSelector = new SuppressedRequiredMemberSelector();
+            }
+
+            //Default services
+            config.Services.Replace(typeof(IContentNegotiator),
+                new DefaultContentNegotiator(excludeMatchOnTypeOnly: true));
+            config.Services.RemoveAll(typeof(ModelValidatorProvider), validator=>!(validator is DataAnnotationsModelValidatorProvider));
+        }
+    }
+}
