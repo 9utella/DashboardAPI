c70ee5779803789115c79069dc59c1d72eeb942d
9utell <9utell@gmail.com>
2018-02-03 12:56:49 -0800

    Adding request message ext and auth handler.

--- Dashboard.API/MessageHandlers/DashboardAuthHandler.cs
+++ Dashboard.API/MessageHandlers/DashboardAuthHandler.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Dashboard.API.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WebApiDoodle.Web.MessageHandlers;
 
 namespace Dashboard.API.MessageHandlers
 {
-    class DashboardAuthHandler
+    public class DashboardAuthHandler : BasicAuthenticationHandler
     {
+        protected override Task<IPrincipal> AuthenticateUserAsync(
+            HttpRequestMessage request,
+            string username,
+            string password,
+            CancellationToken cancellationToken)
+        {
+            var membershipService = request.GetMembershipService();
+            var validUserCtx = membershipService
+                .ValidateUser(username, password);
+            return Task.FromResult(validUserCtx.Principal);
+        }
     }
 }
